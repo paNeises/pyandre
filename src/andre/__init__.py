937e13d8655c2f826dda53a0827fe22eec3b1732
Patrick Neises <paNeises@users.noreply.github.com>
2025-11-10 16:50:53 +0100

    Extend Author class, add BlockResult class and utility function

--- src/andre/__init__.py
+++ src/andre/__init__.py
@@ -1,4 +1,82 @@
-def validate_arid_list_unique_ids(arid_list):
+class Author():
+    """
+    A class for representing an author as the list of its authorship record
+    ID's.
+    """
+
+    def __init__(self, arid_list: list[str]):
+        """
+        Checks if the provided list of authorship record ID's contains only
+        unique entries and then creates and Author object with this list of
+        authorship record ID's. If the provided list contains duplicates,
+        an exception is raised.
+
+        Keyword arguments:
+        arid_list: The list of authorship record ID's that belog to the author.
+        """
+        if not validate_arid_list_unique_arids(arid_list):
+            raise ValueError("The given arid_list contains duplicate entries, "
+                             "but creating an Author object with duplicates "
+                             "in the arid_list is forbidden.")
+        self._arid_list: list[str] = arid_list
+
+    def get_arid_list(self) -> list[str]:
+        """
+        Get the list of authorship records that belong to this author.
+        """
+        return self._arid_list
+
+
+class BlockResult():
+    """
+    A class for representing the results of an author name disambiguation
+    system of a block (obtained by a blocking procedure) of authorship records.
+    The class conatins 2 lists of Author objects, one indicating the correct
+    disambiguation result and the other indicating the results obtained by the
+    system that should be evaluated.
+    """
+
+    def __init__(self,
+                 correct_authors: list[Author],
+                 obtained_authors: list[Author]):
+        """
+        Checks that both author lists are valid in the sence that the authors
+        in each list do not share an authorship record and checks if both lists
+        cover the same authorship records. If both checks succeed, a
+        BlockResult object is created. If a check failes, an exception is
+        raised.
+
+        Keyword arguments:
+        correct_authors: The list of correctly disambiguated authors.
+        obtained_authors: The list of authors obtained from the system that
+        should be evaluated.
+        """
+        correct_authors_arids: list[str] =\
+            get_arids_from_author_list(correct_authors)
+        obtained_authors_arids: list[str] =\
+            get_arids_from_author_list(obtained_authors)
+        if not validate_arid_list_unique_arids(correct_authors_arids):
+            raise ValueError("The given correct authors list contains authors "
+                             "that share an authorship, which is forbidden "
+                             "when creating BlockResult object.")
+        if not validate_arid_list_unique_arids(obtained_authors_arids):
+            raise ValueError("The given obtained authors list contains "
+                             "authors that share an authorship, which is "
+                             "forbidden when creating BlockResult object.")
+        if set(correct_authors_arids) != set(obtained_authors_arids):
+            raise ValueError("The set of authorship records accumulated over "
+                             "all authors in the correct_authors list is not "
+                             "equal to the set of authorship records "
+                             "accumulated over all author in the "
+                             "obtained_authors list, which is forbidden when "
+                             "creating a BlockResult onject.")
+        correct_authors_arids.sort()
+        self._correct_authors: list[Author] = correct_authors
+        self._obtained_authors: list[Author] = obtained_authors
+        self._contained_arids: list[str] = correct_authors_arids
+
+
+def validate_arid_list_unique_arids(arid_list: list[str]) -> bool:
     """
     Validate if a given authorship record ID list contains only unique entries.
     Returns True if the entries are unique, False otherwise.
@@ -6,8 +84,7 @@ def validate_arid_list_unique_ids(arid_list):
     Keyword arguments:
     arid_list: The list of authorship record ID's that should be validated.
     """
-
-    arid_set = set()
+    arid_set: set[str] = set()
     for arid in arid_list:
         arid_set.add(arid)
     if len(arid_list) == len(arid_set):
@@ -15,24 +92,17 @@ def validate_arid_list_unique_ids(arid_list):
     return False
 
 
-class Author():
+def get_arids_from_author_list(author_list: list[Author]) -> list[str]:
     """
-    A class for representing an author as the list of its authorship record
-    ID's.
+    Get the list of all authorship records from a list of author objects. This
+    does not check if the resulting list conatins duplicates, which would
+    imply that the same authorship record is present in different authors
+    in the list.
+
+    Keyword arguments:
+    author_list: The list of author that serves as a starting point.
     """
-
-    def __init__(self, arid_list):
-        """
-        Checks if the provided list of authorship record ID's contains only
-        unique entries and then creates and Author object with this list of
-        authorship record ID's. If the provided list contains duplicates,
-        an exception is thrown.
-
-        Keyword arguments:
-        arid_list: The list of authorship record ID's that belog to the author.
-        """
-        if not validate_arid_list_unique_ids(arid_list):
-            raise ValueError("The given arid_list contains duplicate entries, "
-                             "but creating an author with duplicates in the "
-                             "arid_list is forbidden.")
-        self._arid_list = arid_list
+    arid_list: list[str] = []
+    for author in author_list:
+        arid_list += author.get_arid_list()
+    return arid_list
--- test/test__andre.py
+++ test/test__andre.py
@@ -4,26 +4,6 @@ import pytest
 import andre
 
 
-def test_validate_arid_list_unique_ids_1():
-    """
-    Test validate_arid_list_unique_ids with a list containing only unique
-    entries.
-    """
-    arid_list = ["1", "2", "3"]
-    result = andre.validate_arid_list_unique_ids(arid_list)
-    assert result is True
-
-
-def test_validate_arid_list_unique_ids_2():
-    """
-    Test validate_arid_list_unique_ids with a list containing a duplicate
-    entry.
-    """
-    arid_list = ["1", "2", "3", "3"]
-    result = andre.validate_arid_list_unique_ids(arid_list)
-    assert result is False
-
-
 def test_Author_1():
     """
     Test the creation of an Author object with a list containing only unique
@@ -31,7 +11,6 @@ def test_Author_1():
     """
     arid_list = ["1", "2", "3"]
     andre.Author(arid_list)
-    assert True is True
 
 
 def test_Author_2():
@@ -43,3 +22,170 @@ def test_Author_2():
     with pytest.raises(Exception) as exception:
         andre.Author(arid_list)
     assert exception.type == ValueError
+
+
+def test_Author_get_arid_list_1():
+    """
+    Test the get_arid_list function of an Author object.
+    """
+    arid_list = ["1", "2", "3"]
+    author = andre.Author(arid_list)
+    author_arid_list = author.get_arid_list()
+    assert arid_list == author_arid_list
+
+
+def test_BlockResult_1():
+    """
+    Test the creation of a BlockResult object with valid author lists.
+    """
+    arid_list_author_1 = ["1", "2"]
+    arid_list_author_2 = ["3", "4"]
+    arid_list_author_3 = ["1"]
+    arid_list_author_4 = ["2", "3"]
+    arid_list_author_5 = ["4"]
+    author_1 = andre.Author(arid_list_author_1)
+    author_2 = andre.Author(arid_list_author_2)
+    author_3 = andre.Author(arid_list_author_3)
+    author_4 = andre.Author(arid_list_author_4)
+    author_5 = andre.Author(arid_list_author_5)
+    correct_authors = [author_1, author_2]
+    obtained_authors = [author_3, author_4, author_5]
+    andre.BlockResult(correct_authors, obtained_authors)
+
+
+def test_BlockResult_2():
+    """
+    Test the creation of a BlockResult object with invalid author lists, where
+    the correct one contains duplicate authorship records between ist authors.
+    """
+    arid_list_author_1 = ["1", "2", "3"]
+    arid_list_author_2 = ["3", "4"]
+    arid_list_author_3 = ["1"]
+    arid_list_author_4 = ["2", "3"]
+    arid_list_author_5 = ["4"]
+    author_1 = andre.Author(arid_list_author_1)
+    author_2 = andre.Author(arid_list_author_2)
+    author_3 = andre.Author(arid_list_author_3)
+    author_4 = andre.Author(arid_list_author_4)
+    author_5 = andre.Author(arid_list_author_5)
+    correct_authors = [author_1, author_2]
+    obtained_authors = [author_3, author_4, author_5]
+    with pytest.raises(Exception) as exception:
+        andre.BlockResult(correct_authors, obtained_authors)
+    assert exception.type == ValueError
+
+
+def test_BlockResult_3():
+    """
+    Test the creation of a BlockResult object with invalid author lists, where
+    the obtained one contains duplicate authorship records between ist authors.
+    """
+    arid_list_author_1 = ["1", "2"]
+    arid_list_author_2 = ["3", "4"]
+    arid_list_author_3 = ["1", "2"]
+    arid_list_author_4 = ["2", "3"]
+    arid_list_author_5 = ["4"]
+    author_1 = andre.Author(arid_list_author_1)
+    author_2 = andre.Author(arid_list_author_2)
+    author_3 = andre.Author(arid_list_author_3)
+    author_4 = andre.Author(arid_list_author_4)
+    author_5 = andre.Author(arid_list_author_5)
+    correct_authors = [author_1, author_2]
+    obtained_authors = [author_3, author_4, author_5]
+    with pytest.raises(Exception) as exception:
+        andre.BlockResult(correct_authors, obtained_authors)
+    assert exception.type == ValueError
+
+
+def test_BlockResult_4():
+    """
+    Test the creation of a BlockResult object with invalid author lists, where
+    the correct one contains one authorship record not presend in the obtained
+    one.
+    """
+    arid_list_author_1 = ["1", "2"]
+    arid_list_author_2 = ["3", "4", "5"]
+    arid_list_author_3 = ["1"]
+    arid_list_author_4 = ["2", "3"]
+    arid_list_author_5 = ["4"]
+    author_1 = andre.Author(arid_list_author_1)
+    author_2 = andre.Author(arid_list_author_2)
+    author_3 = andre.Author(arid_list_author_3)
+    author_4 = andre.Author(arid_list_author_4)
+    author_5 = andre.Author(arid_list_author_5)
+    correct_authors = [author_1, author_2]
+    obtained_authors = [author_3, author_4, author_5]
+    with pytest.raises(Exception) as exception:
+        andre.BlockResult(correct_authors, obtained_authors)
+    assert exception.type == ValueError
+
+
+def test_BlockResult_5():
+    """
+    Test the creation of a BlockResult object with invalid author lists, where
+    the obtained one contains one authorship record not presend in the correct
+    one.
+    """
+    arid_list_author_1 = ["1", "2"]
+    arid_list_author_2 = ["3", "4"]
+    arid_list_author_3 = ["1"]
+    arid_list_author_4 = ["2", "3"]
+    arid_list_author_5 = ["4", "5"]
+    author_1 = andre.Author(arid_list_author_1)
+    author_2 = andre.Author(arid_list_author_2)
+    author_3 = andre.Author(arid_list_author_3)
+    author_4 = andre.Author(arid_list_author_4)
+    author_5 = andre.Author(arid_list_author_5)
+    correct_authors = [author_1, author_2]
+    obtained_authors = [author_3, author_4, author_5]
+    with pytest.raises(Exception) as exception:
+        andre.BlockResult(correct_authors, obtained_authors)
+    assert exception.type == ValueError
+
+
+def test_validate_arid_list_unique_arids_1():
+    """
+    Test validate_arid_list_unique_ids with a list containing only unique
+    entries.
+    """
+    arid_list = ["1", "2", "3"]
+    result = andre.validate_arid_list_unique_arids(arid_list)
+    assert result is True
+
+
+def test_validate_arid_list_unique_arids_2():
+    """
+    Test validate_arid_list_unique_ids with a list containing a duplicate
+    entry.
+    """
+    arid_list = ["1", "2", "3", "3"]
+    result = andre.validate_arid_list_unique_arids(arid_list)
+    assert result is False
+
+
+def test_get_arids_from_author_list_1():
+    """
+    Test get_arids_from_author_list with two authors that do not share an
+    authorship record.
+    """
+    arid_list_author_1 = ["1", "2", "5"]
+    arid_list_author_2 = ["3", "4"]
+    author_1 = andre.Author(arid_list_author_1)
+    author_2 = andre.Author(arid_list_author_2)
+    author_list = [author_1, author_2]
+    result = andre.get_arids_from_author_list(author_list)
+    assert result == ["1", "2", "5", "3", "4"]
+
+
+def test_get_arids_from_author_list_2():
+    """
+    Test get_arids_from_author_list with two authors that share an authorship
+    record.
+    """
+    arid_list_author_1 = ["1", "2", "3"]
+    arid_list_author_2 = ["3", "4"]
+    author_1 = andre.Author(arid_list_author_1)
+    author_2 = andre.Author(arid_list_author_2)
+    author_list = [author_1, author_2]
+    result = andre.get_arids_from_author_list(author_list)
+    assert result == ["1", "2", "3", "3", "4"]
