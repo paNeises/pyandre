4467d68666c93f19c26ad9af992951d9f52d2083
Patrick Neises <paNeises@users.noreply.github.com>
2025-11-10 14:37:56 +0100

    Add util function and Author class

--- src/andre/__init__.py
+++ src/andre/__init__.py
@@ -1,2 +1,38 @@
-def test(text):
-    return text
+def validate_arid_list_unique_ids(arid_list):
+    """
+    Validate if a given authorship record ID list contains only unique entries.
+    Returns True if the entries are unique, False otherwise.
+
+    Keyword arguments:
+    arid_list: The list of authorship record ID's that should be validated.
+    """
+
+    arid_set = set()
+    for arid in arid_list:
+        arid_set.add(arid)
+    if len(arid_list) == len(arid_set):
+        return True
+    return False
+
+
+class Author():
+    """
+    A class for representing an author as the list of its authorship record
+    ID's.
+    """
+
+    def __init__(self, arid_list):
+        """
+        Checks if the provided list of authorship record ID's contains only
+        unique entries and then creates and Author object with this list of
+        authorship record ID's. If the provided list contains duplicates,
+        an exception is thrown.
+
+        Keyword arguments:
+        arid_list: The list of authorship record ID's that belog to the author.
+        """
+        if not validate_arid_list_unique_ids(arid_list):
+            raise ValueError("The given arid_list contains duplicate entries, "
+                             "but creating an author with duplicates in the "
+                             "arid_list is forbidden.")
+        self._arid_list = arid_list
--- test/test__andre.py
+++ test/test__andre.py
@@ -1,6 +1,45 @@
+import pytest
+
+
 import andre
 
 
-def test_test():
-    var = andre.test("Hallo")
-    assert var == "Hallo"
+def test_validate_arid_list_unique_ids_1():
+    """
+    Test validate_arid_list_unique_ids with a list containing only unique
+    entries.
+    """
+    arid_list = ["1", "2", "3"]
+    result = andre.validate_arid_list_unique_ids(arid_list)
+    assert result is True
+
+
+def test_validate_arid_list_unique_ids_2():
+    """
+    Test validate_arid_list_unique_ids with a list containing a duplicate
+    entry.
+    """
+    arid_list = ["1", "2", "3", "3"]
+    result = andre.validate_arid_list_unique_ids(arid_list)
+    assert result is False
+
+
+def test_Author_1():
+    """
+    Test the creation of an Author object with a list containing only unique
+    authorship record entries.
+    """
+    arid_list = ["1", "2", "3"]
+    andre.Author(arid_list)
+    assert True is True
+
+
+def test_Author_2():
+    """
+    Test the creation of an Author object with a list containing duplicate
+    authorship record entries.
+    """
+    arid_list = ["1", "2", "3", "3"]
+    with pytest.raises(Exception) as exception:
+        andre.Author(arid_list)
+    assert exception.type == ValueError
