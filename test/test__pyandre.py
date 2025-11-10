8aef3a7e90ee4f45566c2ae68ec2f6b8448e50c5
Patrick Neises <paNeises@users.noreply.github.com>
2025-11-10 17:33:25 +0100

    Rename package

--- test/test__pyandre.py
+++ test/test__pyandre.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-import andre
+import pyandre
 
 
 def test_Author_1():
@@ -10,7 +10,7 @@ def test_Author_1():
     authorship record entries.
     """
     arid_list = ["1", "2", "3"]
-    andre.Author(arid_list)
+    pyandre.Author(arid_list)
 
 
 def test_Author_2():
@@ -20,7 +20,7 @@ def test_Author_2():
     """
     arid_list = ["1", "2", "3", "3"]
     with pytest.raises(Exception) as exception:
-        andre.Author(arid_list)
+        pyandre.Author(arid_list)
     assert exception.type == ValueError
 
 
@@ -29,7 +29,7 @@ def test_Author_get_arid_list_1():
     Test the get_arid_list function of an Author object.
     """
     arid_list = ["1", "2", "3"]
-    author = andre.Author(arid_list)
+    author = pyandre.Author(arid_list)
     author_arid_list = author.get_arid_list()
     assert arid_list == author_arid_list
 
@@ -43,14 +43,14 @@ def test_BlockResult_1():
     arid_list_author_3 = ["1"]
     arid_list_author_4 = ["2", "3"]
     arid_list_author_5 = ["4"]
-    author_1 = andre.Author(arid_list_author_1)
-    author_2 = andre.Author(arid_list_author_2)
-    author_3 = andre.Author(arid_list_author_3)
-    author_4 = andre.Author(arid_list_author_4)
-    author_5 = andre.Author(arid_list_author_5)
+    author_1 = pyandre.Author(arid_list_author_1)
+    author_2 = pyandre.Author(arid_list_author_2)
+    author_3 = pyandre.Author(arid_list_author_3)
+    author_4 = pyandre.Author(arid_list_author_4)
+    author_5 = pyandre.Author(arid_list_author_5)
     correct_authors = [author_1, author_2]
     obtained_authors = [author_3, author_4, author_5]
-    andre.BlockResult(correct_authors, obtained_authors)
+    pyandre.BlockResult(correct_authors, obtained_authors)
 
 
 def test_BlockResult_2():
@@ -63,15 +63,15 @@ def test_BlockResult_2():
     arid_list_author_3 = ["1"]
     arid_list_author_4 = ["2", "3"]
     arid_list_author_5 = ["4"]
-    author_1 = andre.Author(arid_list_author_1)
-    author_2 = andre.Author(arid_list_author_2)
-    author_3 = andre.Author(arid_list_author_3)
-    author_4 = andre.Author(arid_list_author_4)
-    author_5 = andre.Author(arid_list_author_5)
+    author_1 = pyandre.Author(arid_list_author_1)
+    author_2 = pyandre.Author(arid_list_author_2)
+    author_3 = pyandre.Author(arid_list_author_3)
+    author_4 = pyandre.Author(arid_list_author_4)
+    author_5 = pyandre.Author(arid_list_author_5)
     correct_authors = [author_1, author_2]
     obtained_authors = [author_3, author_4, author_5]
     with pytest.raises(Exception) as exception:
-        andre.BlockResult(correct_authors, obtained_authors)
+        pyandre.BlockResult(correct_authors, obtained_authors)
     assert exception.type == ValueError
 
 
@@ -85,15 +85,15 @@ def test_BlockResult_3():
     arid_list_author_3 = ["1", "2"]
     arid_list_author_4 = ["2", "3"]
     arid_list_author_5 = ["4"]
-    author_1 = andre.Author(arid_list_author_1)
-    author_2 = andre.Author(arid_list_author_2)
-    author_3 = andre.Author(arid_list_author_3)
-    author_4 = andre.Author(arid_list_author_4)
-    author_5 = andre.Author(arid_list_author_5)
+    author_1 = pyandre.Author(arid_list_author_1)
+    author_2 = pyandre.Author(arid_list_author_2)
+    author_3 = pyandre.Author(arid_list_author_3)
+    author_4 = pyandre.Author(arid_list_author_4)
+    author_5 = pyandre.Author(arid_list_author_5)
     correct_authors = [author_1, author_2]
     obtained_authors = [author_3, author_4, author_5]
     with pytest.raises(Exception) as exception:
-        andre.BlockResult(correct_authors, obtained_authors)
+        pyandre.BlockResult(correct_authors, obtained_authors)
     assert exception.type == ValueError
 
 
@@ -108,15 +108,15 @@ def test_BlockResult_4():
     arid_list_author_3 = ["1"]
     arid_list_author_4 = ["2", "3"]
     arid_list_author_5 = ["4"]
-    author_1 = andre.Author(arid_list_author_1)
-    author_2 = andre.Author(arid_list_author_2)
-    author_3 = andre.Author(arid_list_author_3)
-    author_4 = andre.Author(arid_list_author_4)
-    author_5 = andre.Author(arid_list_author_5)
+    author_1 = pyandre.Author(arid_list_author_1)
+    author_2 = pyandre.Author(arid_list_author_2)
+    author_3 = pyandre.Author(arid_list_author_3)
+    author_4 = pyandre.Author(arid_list_author_4)
+    author_5 = pyandre.Author(arid_list_author_5)
     correct_authors = [author_1, author_2]
     obtained_authors = [author_3, author_4, author_5]
     with pytest.raises(Exception) as exception:
-        andre.BlockResult(correct_authors, obtained_authors)
+        pyandre.BlockResult(correct_authors, obtained_authors)
     assert exception.type == ValueError
 
 
@@ -131,15 +131,15 @@ def test_BlockResult_5():
     arid_list_author_3 = ["1"]
     arid_list_author_4 = ["2", "3"]
     arid_list_author_5 = ["4", "5"]
-    author_1 = andre.Author(arid_list_author_1)
-    author_2 = andre.Author(arid_list_author_2)
-    author_3 = andre.Author(arid_list_author_3)
-    author_4 = andre.Author(arid_list_author_4)
-    author_5 = andre.Author(arid_list_author_5)
+    author_1 = pyandre.Author(arid_list_author_1)
+    author_2 = pyandre.Author(arid_list_author_2)
+    author_3 = pyandre.Author(arid_list_author_3)
+    author_4 = pyandre.Author(arid_list_author_4)
+    author_5 = pyandre.Author(arid_list_author_5)
     correct_authors = [author_1, author_2]
     obtained_authors = [author_3, author_4, author_5]
     with pytest.raises(Exception) as exception:
-        andre.BlockResult(correct_authors, obtained_authors)
+        pyandre.BlockResult(correct_authors, obtained_authors)
     assert exception.type == ValueError
 
 
@@ -149,10 +149,10 @@ def test_BlockResult_get_contained_arids_1():
     """
     arid_list_author_1 = ["1", "3"]
     arid_list_author_2 = ["2", "4"]
-    author_1 = andre.Author(arid_list_author_1)
-    author_2 = andre.Author(arid_list_author_2)
+    author_1 = pyandre.Author(arid_list_author_1)
+    author_2 = pyandre.Author(arid_list_author_2)
     author_list = [author_1, author_2]
-    blockresult = andre.BlockResult(author_list, author_list)
+    blockresult = pyandre.BlockResult(author_list, author_list)
     result = blockresult.get_contained_arids()
     assert result == ["1", "2", "3", "4"]
 
@@ -163,7 +163,7 @@ def test_validate_arid_list_unique_arids_1():
     entries.
     """
     arid_list = ["1", "2", "3"]
-    result = andre.validate_arid_list_unique_arids(arid_list)
+    result = pyandre.validate_arid_list_unique_arids(arid_list)
     assert result is True
 
 
@@ -173,7 +173,7 @@ def test_validate_arid_list_unique_arids_2():
     entry.
     """
     arid_list = ["1", "2", "3", "3"]
-    result = andre.validate_arid_list_unique_arids(arid_list)
+    result = pyandre.validate_arid_list_unique_arids(arid_list)
     assert result is False
 
 
@@ -184,10 +184,10 @@ def test_get_arids_from_author_list_1():
     """
     arid_list_author_1 = ["1", "2", "5"]
     arid_list_author_2 = ["3", "4"]
-    author_1 = andre.Author(arid_list_author_1)
-    author_2 = andre.Author(arid_list_author_2)
+    author_1 = pyandre.Author(arid_list_author_1)
+    author_2 = pyandre.Author(arid_list_author_2)
     author_list = [author_1, author_2]
-    result = andre.get_arids_from_author_list(author_list)
+    result = pyandre.get_arids_from_author_list(author_list)
     assert result == ["1", "2", "5", "3", "4"]
 
 
@@ -198,8 +198,8 @@ def test_get_arids_from_author_list_2():
     """
     arid_list_author_1 = ["1", "2", "3"]
     arid_list_author_2 = ["3", "4"]
-    author_1 = andre.Author(arid_list_author_1)
-    author_2 = andre.Author(arid_list_author_2)
+    author_1 = pyandre.Author(arid_list_author_1)
+    author_2 = pyandre.Author(arid_list_author_2)
     author_list = [author_1, author_2]
-    result = andre.get_arids_from_author_list(author_list)
+    result = pyandre.get_arids_from_author_list(author_list)
     assert result == ["1", "2", "3", "3", "4"]
